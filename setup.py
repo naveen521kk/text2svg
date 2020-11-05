17bee6b94d4be7273671805954fed7fa5a20dc2e
Naveen M K <naveen@syrusdark.website>
2020-11-05 10:16:51 +0530

    small change for libs

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def get_library_config(name):
     """
     try:
         proc = Popen(
-            ["pkg-config", "--cflags", "--libs", name], stdout=PIPE, stderr=PIPE
+            ["pkg-config", "--cflags", "--libs-only-l", name], stdout=PIPE, stderr=PIPE
         )
     except OSError:
         print("pkg-config is required for building text2svg")
