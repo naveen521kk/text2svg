0783cfc55b2a2ab82d0728674a0f60d007163175
Syrus Dark <naveen@syrusdark.website>
2020-11-06 00:59:47 +0530

    Try building Windows Wheels

--- scripts/download_dlls.py
+++ scripts/download_dlls.py
@@ -8,25 +8,50 @@ from pathlib import Path
 from urllib.request import urlretrieve
 from urllib.request import urlretrieve as download
 import tempfile
+import struct
+import re
+def get_platform():
+    if (struct.calcsize("P") * 8) == 32:
+        return "x86"
+    else:
+        return "amd64"
 
 logging.basicConfig(format="%(levelname)s - %(message)s",level=logging.DEBUG)
 
-final_location = Path(r"D:/bin/")
+final_location = Path(r"C:\cibw\vendor")
 download_location = Path(tempfile.mkdtemp())
-if download_location.exists():
-    logging.info("Download Location already exists clearing it...")
-    shutil.rmtree(str(download_location))
+if final_location.exists():
+    logging.info("Final Location already exists clearing it...")
+    shutil.rmtree(str(final_location))
 
-os.makedirs(download_location)
+os.makedirs(final_location)
 download_file=download_location / "build.zip"
 logging.info("Downloading Pango and Cairo Binaries for Windows...")
 download(url="https://ci.appveyor.com/api/projects/naveen521kk/pango-cairo-build/artifacts/pango-cairo-build.zip?job=image:%20Visual%20Studio%202017",filename=download_file)
 logging.info(f"Download complete. Saved to {download_file}.")
-logging.info(f"Extracting {download_file}...")
+logging.info(f"Extracting {download_file} to {download_location}...")
 with zipfile.ZipFile(download_file, mode="r", compression=zipfile.ZIP_DEFLATED) as file:
     file.extractall(download_location)
 os.remove(download_file)
 logging.info("Completed Extracting.")
-logging.info("Moving Files.")
-
-shutil.move(str(download_location / "build") , str(final_location))
+plat = get_platform()
+logging.debug(f"Found Platform as {plat}")
+logging.info("Moving Files accordingly.")
+plat_location = download_location / "build" / plat
+for src_file in plat_location.glob('*'):
+    logging.debug(f"Moving {src_file} to {final_location}...")
+    shutil.move(str(src_file), str(final_location))
+logging.info("Moving files Completed")
+logging.info("Fixing .pc files")
+import re
+rex=re.compile("^prefix=(.*)")
+def new_place(some):
+    return f"prefix={str(final_location)}"
+pc_files=final_location / "lib" / "pkgconfig"
+for i in pc_files.glob("*.pc"):
+    logging.info(f"Writing {i}")
+    with open(i) as f:
+        content = f.read()
+        final = rex.sub(new_place,content)
+    with open(i,"w") as f:
+        f.write(final)
