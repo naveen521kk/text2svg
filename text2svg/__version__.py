1b60aab66bfc046fb694df4b50f2bb363698f726
Syrus Dark <naveen@syrusdark.website>
2020-11-06 14:57:41 +0530

    Bump Version

--- text2svg/__version__.py
+++ text2svg/__version__.py
@@ -1,6 +1,6 @@
 __title__ = "text2svg"
-__version__ = "0.0.1"
-__summary__ = "convert text to svg"
+__version__ = "0.1.0"
+__summary__ = "Convert text to svg"
 __uri__ = "https://github.com/naveen521kk/text2svg"
 __author__ = "Naveen M K"
 __license__ = "GPLv3"
